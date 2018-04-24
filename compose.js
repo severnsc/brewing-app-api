5178c4ddb54ef662bb0346ac8f180e39c010b6d5
severnsc <severnsc@gmail.com>
2018-04-24 09:49:35 -0400

    Separate use case functions in compose file

--- compose.js
+++ compose.js
@@ -47,11 +47,16 @@ import {
 import { sendMessage } from './adapters/messagingAdapter'
 
 export { hashPassword }
+
+//User
 export const getUser = core.getUserUseCase(findUserById)
 export const createUser = core.createUserUseCase(isUsernameUnique)(_createUser)(hashPassword)
 export const updateUser = core.updateUserUseCase(findUserById)(saveUser)
 export const authenticateUser = core.authenticateUserUseCase(findUserByUsername)(hashPassword)
 export const deleteUser = core.deleteUserUseCase(_deleteUser)
+
+//Timer
+export const getTimer = core.getTimerUseCase(findTimerById)
 export const createTimer = core.createTimerUseCase(_createTimer)
 export const startTimer = core.startTimerUseCase(findTimerById)(saveTimer)
 export const stopTimer = core.stopTimerUseCase(findTimerById)(saveTimer)
@@ -60,17 +65,22 @@ export const resetTimer = core.resetTimerUseCase(findTimerById)(saveTimer)
 export const updateTimer = core.updateTimerUseCase(findTimerById)(saveTimer)
 export const deleteTimer = core.deleteTimerUseCase(_deleteTimer)
 export const getTimersByUserId = core.getTimersByUserIdUseCase(userExists)(findTimersByUserId)
+
+//Inventory
 export const createInventory = core.createInventoryUseCase(_createInventory)
 export const updateInventory = core.updateInventoryUseCase(findInventoryById)(saveInventory)
 export const deleteInventory = core.deleteInventoryUseCase(_deleteInventory)
 export const getInventoriesByUserId = core.getInventoriesByUserIdUseCase(userExists)(findInventoriesByUserId)
-export const getTimer = core.getTimerUseCase(findTimerById)
 export const getInventory = core.getInventoryUseCase(findInventoryById)
+
+//Timer Alert
 export const createTimerAlert = core.createTimerAlertUseCase(_createTimerAlert)
 export const updateTimerAlert = core.updateTimerAlertUseCase(findTimerAlertById)(saveTimerAlert)
 export const activateTimerAlert = core.activateTimerAlertUseCase(findTimerAlertById)(saveTimerAlert)(sendMessage)
 export const deleteTimerAlert = core.deleteTimerAlertUseCase(_deleteTimerAlert)
 export const getTimerAlertsByTimerId = core.getTimerAlertsByTimerIdUseCase(timerExists)(findTimerAlertsByTimerId)
+
+//Inventory Item
 export const createInventoryItem = core.createInventoryItemUseCase(_createInventoryItem)(addToInventory)
 export const updateInventoryItem = core.updateInventoryItemUseCase(findInventoryItemById)(saveInventoryItem)
 export const deleteInventoryItem = core.deleteInventoryItemUseCase(_deleteInventoryItem)
