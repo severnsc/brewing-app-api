284e0211fc12260aadc58f3692a7ca7d522a1bd9
severnsc <severnsc@gmail.com>
2018-05-01 21:36:08 -0400

    Users can only mutate their own data

--- schema/resolvers/mutation.js
+++ schema/resolvers/mutation.js
@@ -5,12 +5,15 @@ import {
   authenticateUser,
   deleteUser,
   hashPassword,
+  getInventory,
   createInventory,
   updateInventory,
   deleteInventory,
+  getInventoryItem,
   createInventoryItem,
   updateInventoryItem,
   deleteInventoryItem,
+  getTimer,
   createTimer,
   startTimer,
   stopTimer,
@@ -18,6 +21,7 @@ import {
   resetTimer,
   updateTimer,
   deleteTimer,
+  getTimerAlert,
   createTimerAlert,
   updateTimerAlert,
   activateTimerAlert,
@@ -30,20 +34,24 @@ export default {
     return user
   },
 
-  updateUser: (_, { id, userName, password}) => {
-    let updatePropsObj = {}
+  updateUser: (_, { id, userName, password}, ctx) => {
+    if(ctx.user.id === id){
+      let updatePropsObj = {}
     
-    if(userName){
-      updatePropsObj.userName = userName
-    }
+      if(userName){
+        updatePropsObj.userName = userName
+      }
 
-    if(password){
-      const hashedPassword = hashPassword(password)
-      updatePropsObj.hashedPassword = hashedPassword
-    }
+      if(password){
+        const hashedPassword = hashPassword(password)
+        updatePropsObj.hashedPassword = hashedPassword
+      }
 
-    const updatedUser = updateUser(id, updatePropsObj)
-    return updatedUser
+      const updatedUser = updateUser(id, updatePropsObj)
+      return updatedUser
+    }else{
+      return null
+    }
   },
 
   authenticateUser: (_, { userName, password }) => {
@@ -51,136 +59,244 @@ export default {
     return authenticatedUser
   },
 
-  deleteUser: (_, { id }) => {
-    const deletedUser = deleteUser(id)
-    return deletedUser
+  deleteUser: (_, { id }, ctx) => {
+    if(ctx.user.id === id){
+      const deletedUser = deleteUser(id)
+      return deletedUser
+    }else{
+      return null
+    }
   },
 
-  createInventory: (_, { name, userId }) => {
-    const inventory = createInventory(name, userId)
-    return inventory
+  createInventory: (_, { name, userId }, ctx) => {
+    if(ctx.user.id === userId){
+      const inventory = createInventory(name, userId)
+      return inventory
+    }else{
+      return null
+    }
   },
 
-  updateInventory: (_, { id, name }) => {
-    const updatedInventory = updateInventory(id, {name})
-    return updatedInventory
-  },
-
-  deleteInventory: (_, { id }) => {
-    const deletedInventory = deleteInventory(id)
-    return deletedInventory
-  },
-
-  createInventoryItem: (_, { inventoryId, object, quantityUnit, currentQuantity, reorderQuantity, reorderThreshold, costUnit, unitCost, reorderCost, lastReorderDate, deliveryDate, createdAt, updatedAt }) => {
-    const inventoryItem = createInventoryItem(inventoryId, object, quantityUnit, currentQuantity, reorderQuantity, reorderThreshold, costUnit, unitCost, reorderCost, lastReorderDate, deliveryDate, createdAt, updatedAt)
-    return inventoryItem
-  },
-
-  updateInventoryItem: (_, { id, inventoryId, object, quantityUnit, currentQuantity, reorderQuantity, reorderThreshold, costUnit, unitCost, reorderCost, lastReorderDate, deliveryDate }) => {
-    let updatePropsObj = {}
-
-    const args = [{inventoryId}, {object}, {quantityUnit}, {currentQuantity}, {reorderQuantity}, {reorderThreshold}, {costUnit}, {unitCost}, {reorderCost}, {lastReorderDate}, {deliveryDate}]
-
-    const argKeys = args.map(arg => Object.keys(arg)[0])
-
-    args.forEach((arg, index) => {
-      if(arg[argKeys[index]]){
-        updatePropsObj = Object.assign({}, updatePropsObj, arg)
-      }else if(arg[argKeys[index]] === 0){
-        updatePropsObj = Object.assign({}, updatePropsObj, arg)
-      }else if(['lastReorderDate', 'deliveryDate'].some(str => str === argKeys[index])){
-        if(arg[argKeys[index]] === null){
-          updatePropsObj = Object.assign({}, updatePropsObj, arg)
-        }
+  updateInventory: (_, { id, name }, ctx) => {
+    return getInventory(id).then(inventory => {
+      if(ctx.user.id === inventory.userId){
+        const updatedInventory = updateInventory(id, {name})
+        return updatedInventory
+      }else{
+        return null
       }
-    })
-
-    const updatedInventoryItem = updateInventoryItem(id, updatePropsObj)
-    return updatedInventoryItem
+    }).catch(e => e)
   },
 
-  deleteInventoryItem: (_, { id }) => {
-    const deletedInventoryItem = deleteInventoryItem(id)
-    return deletedInventoryItem
+  deleteInventory: (_, { id }, ctx) => {
+    return getInventory(id).then(inventory => {
+      if(ctx.user.id === inventory.userId){
+        const deletedInventory = deleteInventory(id)
+        return deletedInventory
+      }else{
+        return null
+      }
+    }).catch(e => e)
   },
 
-  createTimer: (_, { userId, duration, intervalDuration }) => {
-    const timer = createTimer(userId, duration, intervalDuration)
-    return timer
+  createInventoryItem: (_, { inventoryId, object, quantityUnit, currentQuantity, reorderQuantity, reorderThreshold, costUnit, unitCost, reorderCost, lastReorderDate, deliveryDate, createdAt, updatedAt }, ctx) => {
+    return getInventory(inventoryId).then(inventory => {
+      if(ctx.user.id === inventory.userId){
+        const inventoryItem = createInventoryItem(inventoryId, object, quantityUnit, currentQuantity, reorderQuantity, reorderThreshold, costUnit, unitCost, reorderCost, lastReorderDate, deliveryDate, createdAt, updatedAt)
+        return inventoryItem
+      }else{
+        return null
+      }
+    }).catch(e => e)
   },
 
-  startTimer: (_, { id }) => {
-    const timer = startTimer(id)
-    return timer
+  updateInventoryItem: (_, { id, inventoryId, object, quantityUnit, currentQuantity, reorderQuantity, reorderThreshold, costUnit, unitCost, reorderCost, lastReorderDate, deliveryDate }, ctx) => {
+    return getInventory(inventoryId).then(inventory => {
+      if(ctx.user.id === inventory.userId){
+        let updatePropsObj = {}
+
+        const args = [{inventoryId}, {object}, {quantityUnit}, {currentQuantity}, {reorderQuantity}, {reorderThreshold}, {costUnit}, {unitCost}, {reorderCost}, {lastReorderDate}, {deliveryDate}]
+
+        const argKeys = args.map(arg => Object.keys(arg)[0])
+
+        args.forEach((arg, index) => {
+          if(arg[argKeys[index]]){
+            updatePropsObj = Object.assign({}, updatePropsObj, arg)
+          }else if(arg[argKeys[index]] === 0){
+            updatePropsObj = Object.assign({}, updatePropsObj, arg)
+          }else if(['lastReorderDate', 'deliveryDate'].some(str => str === argKeys[index])){
+            if(arg[argKeys[index]] === null){
+              updatePropsObj = Object.assign({}, updatePropsObj, arg)
+            }
+          }
+        })
+
+        const updatedInventoryItem = updateInventoryItem(id, updatePropsObj)
+        return updatedInventoryItem
+      }else{
+        return null
+      }
+    }).catch(e => e)
   },
 
-  stopTimer: (_, { id }) => {
-    const timer = stopTimer(id)
-    return timer
+  deleteInventoryItem: (_, { id }, ctx) => {
+    return getInventoryItem(id).then(inventoryItem => {
+      return getInventory(inventoryItem.invetoryId).then(inventory => {
+        if(ctx.user.id === inventory.userId){
+          const deletedInventoryItem = deleteInventoryItem(id)
+          return deletedInventoryItem
+        }else{
+          return null
+        }
+      }).catch(e => e)
+    }).catch(e => e)
   },
 
-  decrementTimer: (_, { id }) => {
-    const timer = decrementTimer(id)
-    return timer
-  },
-
-  resetTimer: (_, { id }) => {
-    const timer = resetTimer(id)
-    return timer
-  },
-
-  updateTimer: (_, { id, duration, intervalDuration }) => {
-    let updatePropsObj = {}
-
-    if(duration){
-      updatePropsObj.duration = duration
+  createTimer: (_, { userId, duration, intervalDuration }, ctx) => {
+    if(ctx.user.id === userId){
+      const timer = createTimer(userId, duration, intervalDuration)
+      return timer
+    }else{
+      return null
     }
-
-    if(intervalDuration){
-      updatePropsObj.intervalDuration = intervalDuration
-    }
-
-    const timer = updateTimer(id, updatePropsObj)
-    return timer
   },
 
-  deleteTimer: (_, { id }) => {
-    const timer = deleteTimer(id)
-    return timer
+  startTimer: (_, { id }, ctx) => {
+    return getTimer(id).then(timer => {
+      if(ctx.user.id === timer.userId){
+        const startedTimer = startTimer(id)
+        return startedTimer
+      }else{
+        return null
+      }
+    }).catch(e => e)
   },
 
-  createTimerAlert: (_, { timerId, activationTime, message }) => {
-    const timerAlert = createTimerAlert(timerId, activationTime, message)
-    return timerAlert
+  stopTimer: (_, { id }, ctx) => {
+    return getTimer(id).then(timer => {
+      if(ctx.user.id === timer.userId){
+        const stoppedTimer = stopTimer(id)
+        return stoppedTimer
+      }else{
+        return null
+      }
+    }).catch(e => e)
   },
 
-  updateTimerAlert: (id, timerId, activationTime, message) => {
-    let updatePropsObj = {}
-
-    if(timerId){
-      updatePropsObj.timerId = timerId
-    }
-
-    if(typeof activationTime === 'number'){
-      updatePropsObj.activationTime = activationTime
-    }
-
-    if(message){
-      updatePropsObj.message = message
-    }
-
-    const timerAlert = updateTimerAlert(id, updatePropsObj)
-
-    return timerAlert
+  decrementTimer: (_, { id }, ctx) => {
+    return getTimer(id).then(timer => {
+      if(ctx.user.id === timer.userId){
+        const decrementedTimer = decrementTimer(id)
+        return decrementedTimer
+      }else{
+        return null
+      }
+    }).catch(e => e)
   },
 
-  activateTimerAlert: (_, { id }) => {
-    const timerAlert = activateTimerAlert(id)
-    return timerAlert
+  resetTimer: (_, { id }, ctx) => {
+    return getTimer(id).then(timer => {
+      if(ctx.user.id === timer.userId){
+        const resetedTimer = resetTimer(id)
+        return resetedTimer
+      }else{
+        return null
+      }
+    }).catch(e => e)
   },
 
-  deleteTimerAlert: (_, { id }) => {
-    const deletedTimerAlert = deleteTimerAlert(id)
-    return deletedTimerAlert
+  updateTimer: (_, { id, duration, intervalDuration }, ctx) => {
+    return getTimer(id).then(timer => {
+      if(ctx.user.id === timer.userId){
+        let updatePropsObj = {}
+
+        if(duration){
+          updatePropsObj.duration = duration
+        }
+
+        if(intervalDuration){
+          updatePropsObj.intervalDuration = intervalDuration
+        }
+
+        const updatedTimer = updateTimer(id, updatePropsObj)
+        return updatedTimer
+      }else{
+        return null
+      }
+    }).catch(e => e)
+  },
+
+  deleteTimer: (_, { id }, ctx) => {
+    return getTimer(id).then(timer => {
+      if(ctx.user.id === timer.userId){
+        const timer = deleteTimer(id)
+        return timer
+      }else{
+        return null
+      }
+    }).catch(e => e)
+  },
+
+  createTimerAlert: (_, { timerId, activationTime, message }, ctx) => {
+    return getTimer(timerId).then(timer => {
+      if(ctx.user.id === timer.userId){
+        const timerAlert = createTimerAlert(timerId, activationTime, message)
+        return timerAlert
+      }else{
+        return null
+      }
+    }).catch(e => e)
+  },
+
+  updateTimerAlert: (_, { id, timerId, activationTime, message }, ctx) => {
+    return getTimer(timerId).then(timer => {
+      if(ctx.user.id === timer.userId){
+        let updatePropsObj = {}
+
+        if(timerId){
+          updatePropsObj.timerId = timerId
+        }
+
+        if(typeof activationTime === 'number'){
+          updatePropsObj.activationTime = activationTime
+        }
+
+        if(message){
+          updatePropsObj.message = message
+        }
+
+        const timerAlert = updateTimerAlert(id, updatePropsObj)
+
+        return timerAlert
+      }else{
+        return null
+      }
+    }).catch(e => e)
+  },
+
+  activateTimerAlert: (_, { id }, ctx) => {
+    return getTimerAlert(id).then(timerAlert => {
+      return getTimer(timerAlert.timerId).then(timer => {
+        if(ctx.user.id === timer.userId){
+          const activatedTimerAlert = activateTimerAlert(id)
+          return activatedTimerAlert
+        }else{
+          return null
+        }
+      }).catch(e => e)
+    }).catch(e => e)
+  },
+
+  deleteTimerAlert: (_, { id }, ctx) => {
+    return getTimerAlert(id).then(timerAlert => {
+      return getTimer(timerAlert.timerId).then(timer => {
+        if(ctx.user.id === timer.userId){
+          const deletedTimerAlert = deleteTimerAlert(id)
+          return deletedTimerAlert
+        }else{
+          return null
+        }
+      }).catch(e => e)
+    }).catch(e => e)
   }
 }
